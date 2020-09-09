9f59ffd5e4735d208943982f373ecaa19fa114b9
Necritto <onetopyr@mail.ru>
2020-09-09 22:17:39 +0300

    Feature: NavbarItem styles [ref:no-ref]

--- src/components/Navbar/NavbarItem/NavbarItem.tsx
+++ src/components/Navbar/NavbarItem/NavbarItem.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import classes from "./NavbarItem.module.scss";
 
 import { Nodes } from "../Navbar";
+import classnames from "classnames";
 
 export const NavbarItem = ({ nodes }: Nodes) => {
   return (
-    <ul className={classes["inner-menu"]}>
+    <ul
+      className={classnames(classes["inner-menu"], {
+        [classes.prod]: nodes.length === 7,
+        [classes.area]: nodes.length === 13,
+      })}
+    >
       {nodes.map((node) => (
         <li className={classes.menu__item} key={node.id}>
           <a href={node.route}>{node.title}</a>
