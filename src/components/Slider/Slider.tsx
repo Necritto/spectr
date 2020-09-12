cb86e62d2b44d068c487a0e451abd0351b458b1a
Necritto <onetopyr@mail.ru>
2020-09-12 20:57:35 +0300

    Feature: Slider logic [ref:no-ref]

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Slider.module.scss";
 
 import classnames from "classnames";
@@ -40,32 +40,51 @@ export const Slider = () => {
     ],
   };
 
+  const [x, setX] = useState(0);
+  let prettyX: number = (x * -1) / 100;
+
+  const leftSlide = (): void =>
+    x === 0 ? setX(-100 * (data.nodes.length - 1)) : setX(x + 100);
+
+  const rightSlide = (): void =>
+    x === -100 * (data.nodes.length - 1) ? setX(0) : setX(x - 100);
+
   return (
     <div className={classes.slider}>
       <div className={classes.sliderOverlay}>
         <div className={classes["slider-wrap"]}>
           <div className={classnames(classes.arrow, classes["arrow-left"])}>
-            <button className={classes.left}>
+            <button className={classes.left} onClick={leftSlide}>
               <img src={arrowSvg} alt="arrow" />
             </button>
           </div>
           <div className={classes.slider__content}>
             {data.nodes.map((node) => (
-              <SliderItem key={node.id} data={node} />
+              <SliderItem key={node.id} data={node} x={x} />
             ))}
           </div>
           <div className={classnames(classes.arrow, classes["arrow-right"])}>
-            <button className={classes.right}>
+            <button className={classes.right} onClick={rightSlide}>
               <img src={arrowSvg} alt="arrow" />
             </button>
           </div>
         </div>
         <div className={classes.slider__border}>
           <div
-            className={classnames(classes.border, classes["border-active"])}
+            className={classnames(classes.border, {
+              [classes["border-active"]]: prettyX === 0,
+            })}
           />
-          <div className={classes.border} />
-          <div className={classes.border} />
+          <div
+            className={classnames(classes.border, {
+              [classes["border-active"]]: prettyX === 1,
+            })}
+          />
+          <div
+            className={classnames(classes.border, {
+              [classes["border-active"]]: prettyX === 2,
+            })}
+          />
         </div>
       </div>
     </div>
