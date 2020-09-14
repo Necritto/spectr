e992155d40ef71b399c74d9a1440cefb006d437b
Necritto <onetopyr@mail.ru>
2020-09-14 12:14:14 +0300

    Feature: CarouselItem component [ref:no-ref]

---
+++ src/components/Carousel/CarouselItem/CarouselItem.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./CarouselItem.module.scss";
+
+type CarouselItemProps = {
+  card: {
+    id: number;
+    imgUrl: string;
+    title: string;
+    descr: string;
+  };
+};
+
+export const CarouselItem = ({ card }: CarouselItemProps) => {
+  return (
+    <div className={classes.carousel__item}>
+      <div className={classes.card__img}>
+        <img src={card.imgUrl} alt="img" />
+      </div>
+      <div className={classes.card__desc}>
+        <h5>{card.title}</h5>
+        <p>{card.descr}</p>
+      </div>
+    </div>
+  );
+};
