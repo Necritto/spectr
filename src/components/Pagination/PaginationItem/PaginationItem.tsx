b5e6be07351866a224670d17aaf8365c3377e9c0
Necritto <onetopyr@mail.ru>
2020-10-02 18:32:00 +0300

    Feature: update pagination component [ref:no-ref]

--- src/components/Pagination/PaginationItem/PaginationItem.tsx
+++ src/components/Pagination/PaginationItem/PaginationItem.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 import classes from "./PaginationItem.module.scss";
 
+import classnames from "classnames";
+
 type PaginationItemProps = {
   pageData: {
     id: number;
     imgUrl: string;
     title: string;
-    descr: string;
+    descr?: string;
+    date?: string;
   };
+  isNews?: boolean;
 };
 
 export const PaginationItem = ({
   pageData,
+  isNews,
 }: PaginationItemProps): React.ReactElement => {
   return (
-    <div className={classes.pagination__item}>
+    <div
+      className={classnames(classes.pagination__item, {
+        [classes["pagination__item-news"]]: isNews,
+      })}
+    >
       <div className={classes.pagination__img}>
         <img src={pageData.imgUrl} alt={pageData.title} />
       </div>
       <div className={classes.pagination__desc}>
+        {isNews && <span>{pageData.date}</span>}
         <h5>{pageData.title}</h5>
-        <p>{pageData.descr}</p>
+        {!isNews && <p>{pageData.descr}</p>}
       </div>
     </div>
   );
