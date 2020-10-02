50241775385ae13b7026e07f942e809fae1f2fcb
Necritto <onetopyr@mail.ru>
2020-10-02 20:18:05 +0300

    Feature: TeamPageItem component [ref:no-ref]

---
+++ src/pages/TeamPage/TeamPageItem/TeamPageItem.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import classes from "./TeamPageItem.module.scss";
+
+type TeamPageItemProps = {
+  data: {
+    id: number;
+    imgUrl: string;
+    name: string;
+    position: string;
+    descr: string;
+    interests?: string;
+  };
+};
+
+export const TeamPageItem = ({
+  data,
+}: TeamPageItemProps): React.ReactElement => {
+  return (
+    <>
+      <article className={classes.team__item}>
+        <div className={classes.heading}>
+          <div className={classes.team__img}>
+            <img src={data.imgUrl} alt={data.name} />
+          </div>
+          <div className={classes.team__name}>
+            <h2>{data.name}</h2>
+            <span>{data.position}</span>
+          </div>
+        </div>
+        <div className={classes.descr}>
+          <p>{data.descr}</p>
+          {data.interests && (
+            <p>
+              <span>Область научных интересов: </span>
+              {data.interests}
+            </p>
+          )}
+        </div>
+      </article>
+      <div className={classes.line}></div>
+    </>
+  );
+};
