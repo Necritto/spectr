e5b75959b61ab3d9cfb2c1e64eb24fb4c085029c
Necritto <onetopyr@mail.ru>
2020-10-06 21:37:19 +0300

    Fix: clickable images [ref:no-ref]

--- src/components/News/NewsItem/NewsItem.tsx
+++ src/components/News/NewsItem/NewsItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./NewsItem.module.scss";
 
+import { Link } from "react-router-dom";
+
 import { INewsData } from "../../../utils/interfaces/interfaces";
 
 type NewsItemProps = {
@@ -11,7 +13,9 @@ export const NewsItem = ({ item }: NewsItemProps): React.ReactElement => {
   return (
     <div className={classes.newsItem}>
       <div className={classes.newsItem__img}>
-        <img src={item.imgUrl} alt="newsImg" />
+        <Link to={`/news/${item.id}`}>
+          <img src={item.imgUrl} alt="newsImg" />
+        </Link>
       </div>
       <div className={classes.newsItem__descr}>
         <span>{item.date}</span>
